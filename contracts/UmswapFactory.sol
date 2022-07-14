c45afc54d4ffe3516fdd5d522e5a5074a467c6f9
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-07-14 17:07:48 +1000

    wip

--- contracts/UmswapFactory.sol
+++ contracts/UmswapFactory.sol
@@ -14,12 +14,84 @@ pragma solidity ^0.8.0;
 // (c) BokkyPooBah / Bok Consulting Pty Ltd 2022
 // ----------------------------------------------------------------------------
 
-interface IERC20Partial {
+/// @notice https://github.com/optionality/clone-factory/blob/32782f82dfc5a00d103a7e61a17a5dedbd1e8e9d/contracts/CloneFactory.sol
+/*
+The MIT License (MIT)
+
+Copyright (c) 2018 Murray Software, LLC.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+//solhint-disable max-line-length
+//solhint-disable no-inline-assembly
+
+contract CloneFactory {
+
+  function createClone(address target) internal returns (address result) {
+    bytes20 targetBytes = bytes20(target);
+    assembly {
+      let clone := mload(0x40)
+      mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(clone, 0x14), targetBytes)
+      mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+      result := create(0, clone, 0x37)
+    }
+  }
+
+  function isClone(address target, address query) internal view returns (bool result) {
+    bytes20 targetBytes = bytes20(target);
+    assembly {
+      let clone := mload(0x40)
+      mstore(clone, 0x363d3d373d3d3d363d7300000000000000000000000000000000000000000000)
+      mstore(add(clone, 0xa), targetBytes)
+      mstore(add(clone, 0x1e), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+
+      let other := add(clone, 0x40)
+      extcodecopy(query, other, 0, 0x2d)
+      result := and(
+        eq(mload(clone), mload(other)),
+        eq(mload(add(clone, 0xd)), mload(add(other, 0xd)))
+      )
+    }
+  }
+}
+// End CloneFactory.sol
+
+
+/// @notice ERC20 https://eips.ethereum.org/EIPS/eip-20 with optional symbol, name and decimals
+interface IERC20 {
+    function totalSupply() external view returns (uint);
     function balanceOf(address tokenOwner) external view returns (uint balance);
     function allowance(address tokenOwner, address spender) external view returns (uint remaining);
     function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
+
+    function symbol() external view returns (string memory);
+    function name() external view returns (string memory);
+    function decimals() external view returns (uint8);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
+
 
 interface IERC165 {
     function supportsInterface(bytes4 interfaceId) external view returns (bool);
@@ -40,11 +112,13 @@ interface ERC721TokenReceiver {
 contract Owned {
     bytes4 private constant ERC721_INTERFACE = 0x80ac58cd;
 
+    bool initialised;
     address public owner;
 
     event OwnershipTransferred(address indexed from, address indexed to);
     event Withdrawn(address indexed token, uint tokens, uint tokenId);
 
+    error AlreadyInitialised();
     error NotOwner();
 
     modifier onlyOwner {
@@ -54,8 +128,12 @@ contract Owned {
         _;
     }
 
-    constructor() {
-        owner = msg.sender;
+    function initOwned(address _owner) internal {
+        if (initialised) {
+            revert AlreadyInitialised();
+        }
+        owner = address(uint160(_owner));
+        initialised = true;
     }
 
     function transferOwnership(address _newOwner) public onlyOwner {
@@ -79,12 +157,74 @@ contract Owned {
                 IERC721Partial(token).safeTransferFrom(address(this), owner, tokenId);
             } else {
                 if (tokens == 0) {
-                    tokens = IERC20Partial(token).balanceOf(address(this));
+                    tokens = IERC20(token).balanceOf(address(this));
                 }
-                IERC20Partial(token).transfer(owner, tokens);
+                IERC20(token).transfer(owner, tokens);
             }
         }
         emit Withdrawn(token, tokens, tokenId);
+    }
+}
+
+
+/// @notice Basic token = ERC20 + symbol + name + decimals + mint + ownership
+contract BasicToken is IERC20, Owned {
+
+    string _symbol;
+    string _name;
+    uint _decimals;
+    uint _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    function initToken(address tokenOwner, string memory __symbol, string memory __name, uint __decimals) internal {
+        super.initOwned(tokenOwner);
+        _symbol = __symbol;
+        _name = __name;
+        _decimals = __decimals;
+    }
+    function symbol() override external view returns (string memory) {
+        return _symbol;
+    }
+    function name() override external view returns (string memory) {
+        return _name;
+    }
+    function decimals() override external view returns (uint8) {
+        return uint8(_decimals);
+    }
+    function totalSupply() override external view returns (uint) {
+        return _totalSupply - balances[address(0)];
+    }
+    function balanceOf(address tokenOwner) override external view returns (uint balance) {
+        return balances[tokenOwner];
+    }
+    function transfer(address to, uint tokens) override external returns (bool success) {
+        balances[msg.sender] -= tokens;
+        balances[to] += tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+    function approve(address spender, uint tokens) override external returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+    function transferFrom(address from, address to, uint tokens) override external returns (bool success) {
+        balances[from] -= tokens;
+        allowed[from][msg.sender] -= tokens;
+        balances[to] += tokens;
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+    function allowance(address tokenOwner, address spender) override external view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+    function mint(address tokenOwner, uint tokens) external onlyOwner returns (bool success) {
+        balances[tokenOwner] += tokens;
+        _totalSupply += tokens;
+        emit Transfer(address(0), tokenOwner, tokens);
+        return true;
     }
 }
 
@@ -111,13 +251,13 @@ contract Umswap is Owned, ReentrancyGuard, ERC721TokenReceiver {
     event ThankYou(uint tip);
 
     constructor() {
+        super.initOwned(msg.sender);
     }
 
     function onERC721Received(address /*_operator*/, address /*_from*/, uint _tokenId, bytes memory /*_data*/) external override returns(bytes4) {
         emit ThankYou(_tokenId);
         return this.onERC721Received.selector;
     }
-
 }
 
 contract UmswapFactory is Owned {
@@ -126,6 +266,7 @@ contract UmswapFactory is Owned {
     event ThankYou(uint tip);
 
     constructor() {
+        super.initOwned(msg.sender);
     }
 
 
