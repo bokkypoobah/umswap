4688e10809d139379da0ed60b3ba3974fab14b90
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-07-16 01:44:46 +1000

    tidy

--- contracts/UmswapFactory.sol
+++ contracts/UmswapFactory.sol
@@ -416,7 +416,7 @@ contract Umswap is BasicToken, ReentrancyGuard, ERC721TokenReceiver {
 contract UmswapFactory is Owned, CloneFactory {
 
     uint8 constant ZERO = 48;
-    bytes constant UMSYMBOLPREFIX = "Um";
+    bytes constant UMSYMBOLPREFIX = "Ums";
     bytes4 constant ERC721_INTERFACE = 0x80ac58cd;
 
     Umswap public template;
@@ -445,7 +445,7 @@ contract UmswapFactory is Owned, CloneFactory {
                 i++;
             }
         }
-        i = 6;
+        i = 5;
         do {
             unchecked {
                 i--;
