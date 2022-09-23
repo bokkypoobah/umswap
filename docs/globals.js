164174df1e7d7e2c836c27ccd262229bbaaa0554
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-09-23 17:11:44 +1000

    wip

--- docs/globals.js
+++ docs/globals.js
@@ -1,2 +1,8 @@
 const generateRange = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 const delay = ms => new Promise(res => setTimeout(res, ms));
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
