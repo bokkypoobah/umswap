20c1c0fd3bba53c81bdf2e7b9a63714da38eaf3b
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2022-10-01 04:59:49 +1000

    ui wip - messages

--- docs/globals.js
+++ docs/globals.js
@@ -1,3 +1,4 @@
+const ADDRESS0 = "0x0000000000000000000000000000000000000000";
 const generateRange = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 const delay = ms => new Promise(res => setTimeout(res, ms));
 function handleErrors(response) {
